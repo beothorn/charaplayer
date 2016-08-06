8c3c6cb9f96b9639631f7c1af38524fbe13c897d
Lucas <lucass@gmail.com>
2016-08-05 22:44:25 -0300

    Smallest json needed for animation

--- js/src/Main.js
+++ js/src/Main.js
@@ -1,40 +1,20 @@
-console.log("Hello");
-
 var renderer = PIXI.autoDetectRenderer(800, 600);
 document.body.appendChild(renderer.view);
 
-// create the root of the scene graph
 var stage = new PIXI.Container();
 
 PIXI.loader
     .add('guy','media/guy.json')
     .load(onAssetsLoaded);
 
-var movie;
+function onAssetsLoaded(load, resources)
+{    
+    var upWalkFrames = resources.guy.data.animations["upWalk"];
 
-function onAssetsLoaded()
-{
-    /**
-    var pathTextures = [];
-    for(var col = 0; col < 3; col++){
-        pathTextures[col] = [];
-        for(var line = 0; line < 4; line++){
-            pathTextures[col][line] = new PIXI.Texture(PIXI.utils.TextureCache['media/guy.png']);
-            pathTextures[col][line].frame = new PIXI.Rectangle(72*col, 96*line, 72, 96);
-        }
-    }
-    **/
+    var upWalk = upWalkFrames.map(PIXI.Texture.fromFrame);
 
-    var upWalk = [
-        PIXI.Texture.fromFrame('upLeft'),
-        PIXI.Texture.fromFrame('upStopped'),
-        PIXI.Texture.fromFrame('upRight'),
-        PIXI.Texture.fromFrame('upStopped')
-    ];
-
-    movie = new PIXI.extras.MovieClip(upWalk);
-    movie.position.set(300);
-    movie.anchor.set(0.5);
+    var movie = new PIXI.extras.MovieClip(upWalk);
+    movie.position.set(0,0);
     movie.animationSpeed = 0.1;
     movie.play();
 
@@ -44,8 +24,6 @@ function onAssetsLoaded()
 }
 
 function animate() {
-    // render the stage container
     renderer.render(stage);
-
     requestAnimationFrame(animate);
 }
